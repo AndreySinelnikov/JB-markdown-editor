075d24e187376050fc73570820e9bc4c800a0ff7
Andrey Sinelnikov <sinelnikov_a@yahoo.com>
2021-03-20 12:37:02 +0300

    add command input

--- editor.py
+++ editor.py
@@ -1,13 +1,18 @@
+def main():
+    formatters = ("plain", "bold", "italic", "link", "inline-code",
+                 "ordered-list", "unordered-list", "new-line", "header")
 
-sol = """
-# John Lennon
-or ***John Winston Ono Lennon,*** was one of *The Beatles*.
-Here are the songs he wrote I like the most:
-* Imagine
-* Norwegian Wood
-* Come Together
-* In My Life
-* ~~Hey Jude~~(that was *McCartney*)
-"""
+    commands = ("!help", "!done")
 
-print(sol)
+    while True:
+        user_choice = input("- Choose a formatter:")
+        if user_choice == "!help":
+            print("Available formatters:", ' '.join(formatters))
+            print("Available commands: ", ' '.join(commands))
+        elif user_choice == "!done":
+            break
+        elif user_choice not in formatters:
+            print('Unknown formatting type or command. Please try again')
+
+if __name__ == "__main__":
+    main()
